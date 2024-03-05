e1f2d27bf257a082cb1b412f79796670bd1e0d2b
IDEALOGIC\khoa-dang.pham <khoa.pham@publishone.nl>
2024-03-05 17:22:33 +0700

    feat: landing section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
+import { gsap } from "gsap";
+
 import "./style.css";
+
+window.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.getElementById("cursor")!;
+  const cursorSize = cursor.clientWidth;
+
+  this.addEventListener("mousemove", function (e) {
+    const { clientX, clientY } = e;
+
+    gsap.to(cursor, {
+      x: clientX - cursorSize / 2,
+      y: clientY - cursorSize / 2,
+    });
+  });
+});

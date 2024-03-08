b59d19b8cf2adda3359393cf3b820c5c8c02ac37
blackparadise0407 <blackparadise0407@gmail.com>
2024-03-09 02:01:34 +0700

    feat: update minor UI, add build env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import "./style.css";
 
 var grecaptcha: any = (window as any).grecaptcha ?? {};
+var Toastify: any = (window as any).Toastify ?? {};
 
 window.addEventListener("DOMContentLoaded", function () {
   const hash = window.location.hash;
+  if (!hash) {
+    window.location.href = "/#17";
+    return;
+  }
+  const hashString = hash.slice(1);
   const time = document.getElementById("time")!;
   const date1 = document.getElementById("date1")!;
   const date2 = document.getElementById("date2")!;
@@ -11,8 +17,8 @@ window.addEventListener("DOMContentLoaded", function () {
   const location = document.getElementById("location")! as HTMLIFrameElement;
   switch (hash) {
     case "#17":
-      date1.innerText = "17.05.2024";
-      date2.innerText = "17.05.2024";
+      date1.innerText = "- 17.05.2024 -";
+      date2.innerText = "- 17.05.2024 -";
       time.innerText = "19:00 17 tháng 5 năm 2024";
       place.innerText =
         "Hôtel Colline, Số 10 Phan Bội Châu, Phường 1, TP.Đà Lạt";
@@ -20,8 +26,8 @@ window.addEventListener("DOMContentLoaded", function () {
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3903.4407938119366!2d108.43552667587794!3d11.94395343657313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3171135333d87745%3A0xef770fc699bf659b!2sH%C3%B4tel%20Colline!5e0!3m2!1sen!2s!4v1709713910678!5m2!1sen!2s";
       break;
     case "#18":
-      date1.innerText = "18.05.2024";
-      date2.innerText = "18.05.2024";
+      date1.innerText = "- 18.05.2024 -";
+      date2.innerText = "- 18.05.2024 -";
       time.innerText = "11:00 18 tháng 5 năm 2024";
       place.innerText =
         "Restaurant Garden Palace, Số 01 Cô Giang, Phường 9, TP.Đà Lạt";
@@ -50,6 +56,7 @@ window.addEventListener("DOMContentLoaded", function () {
             body: JSON.stringify({
               recaptchaToken: token,
               ...payload,
+              range: hashString,
             }),
           });
           resolve(await resp.json());
@@ -78,5 +85,12 @@ window.addEventListener("DOMContentLoaded", function () {
     e.submitter.classList.remove("loading");
     e.submitter.removeAttribute("disabled");
     e.submitter.removeChild(spinner);
+    Toastify({
+      text: "Cảm ơn bạn đã phản hồi. Chúng tôi rất mong đợi sự có mặt của bạn.",
+      duration: 5000,
+      gravity: "bottom",
+      position: "right",
+      stopOnFocus: true,
+    }).showToast();
   };
 });
